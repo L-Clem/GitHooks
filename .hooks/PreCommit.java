f5226900d0dadd3d73f72e4b6a812a7ff043caac
L-Clem <japanclems287@gmail.com>
2022-11-17 17:05:17 +0100

    Add hooks

---
+++ .hooks/PreCommit.java
@@ -0,0 +1,24 @@
+import java.nio.file.*;
+import java.util.regex.*;  
+
+
+public class PreCommit {
+  public static String readFileAsString(String fileName) throws Exception {
+    String data = "";
+    data = new String(Files.readAllBytes(Paths.get(fileName)));
+    return data;
+  }
+
+  public static void main(String[] args) throws Exception {
+    String data = readFileAsString(args[0]);
+
+    Pattern p = Pattern.compile("^.*\\(.+,*\\): [a-zA-Z1-9 ]*$");
+    Matcher m = p.matcher(data);  
+    boolean b = m.find();  
+
+    if(b)
+      System.exit(0);
+    else
+      System.exit(1);  
+  }
+}
