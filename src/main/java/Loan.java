b8ced0aeee2d5776c2c6abf3e776e605b365029b
L-Clem <japanclems287@gmail.com>
2022-11-28 20:35:29 +0100

    ✅ (dev, test, loan): Add loan tests and update loan variables

--- src/main/java/Loan.java
+++ src/main/java/Loan.java
@@ -1,22 +1,22 @@
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Loan {
-    private final int ID;
-    private final int AMOUNT;
+    private final int id;
+    private final int amount;
     private double outstanding;
 
     Loan(int amount) {
-        this.AMOUNT = amount;
-        this.ID = ThreadLocalRandom.current().nextInt();
+        this.amount = amount;
+        this.id = ThreadLocalRandom.current().nextInt();
         this.outstanding = amount;
     }
 
-    boolean pay(int accountId, int amount) {
-        if (accountId.balance < amount) {
+    boolean pay(Account account, int amount) {
+        if (account.balance < amount) {
             return false;
         }
 
-        accountId.balance -= amount;
+        account.balance -= amount;
         this.outstanding -= amount;
 
         return true;
